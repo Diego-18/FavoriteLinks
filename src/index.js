2b3b612360faf5056bbc1f5f3fbf1d72faab8453
“Diego <ingdiegochavez18@gmail.com>
2021-03-08 20:03:32 -0400

    feat: login-signup

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ const flash = require('connect-flash');
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session');
 const {database} = require('./keys');
+const passport = require('passport');
 
 
 
 // initializations
 const app = express();
+require('./lib/passport');
 
 // settings
 app.set('port', process.env.PORT || 4000);
@@ -36,6 +38,8 @@ app.use(flash());
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 
---
+++ src/lib/helpers.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcryptjs');
+const helpers = {};
+
+// encryt password with bcrypt
+helpers.encryptPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+};
+
+// compare password 
+helpers.matchPassword = async (password, savedPassword) => {
+    try {
+        await bcrypt.compare(password, savedPassword);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+module.exports = helpers;
