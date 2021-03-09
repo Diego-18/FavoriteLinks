b99e4da3de7bab717406ecc50919fdf2298af0cf
“Diego <ingdiegochavez18@gmail.com>
2021-03-09 00:25:32 -0400

    feat: index

--- src/routes/links.js
+++ src/routes/links.js
@@ -13,7 +13,8 @@ router.post('/add', isLoggedIn, async (req, res) =>{
     const newLink = {
         title,
         url,
-        description
+        description,
+        user_id: req.user.id
     };
     //console.log(newLink); 
     await pool.query('INSERT INTO links SET ?', [newLink]);
@@ -23,7 +24,7 @@ router.post('/add', isLoggedIn, async (req, res) =>{
 });
 
 router.get('/', isLoggedIn, async (req, res) =>{ 
-    const links = await pool.query('SELECT * FROM links');
+    const links = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
     //console.log(links);
     res.render('links/list', {links});
 });
